e2f2b25e44044eeed087d93e55230487b24a1247
chinmay5 <chinmayprabhakar55@gmail.com>
2019-11-20 23:55:20 +0100

    using GRU now

--- Networks.py
+++ Networks.py
@@ -6,21 +6,21 @@ from torch.nn.utils.rnn import pad_sequence
 
 class EmbeddingNetwork(nn.Module):
     
-    def __init__(self, vocab_size, hidden_dim, embedding_dim):
+    def __init__(self, vocab_size, hidden_dim, embedding_dim, dropout=0.3):
         
         super(EmbeddingNetwork, self).__init__()
         self.vocab_size = vocab_size
         self.hidden_dim = hidden_dim
         self.embedding_dim = embedding_dim
         self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
-        self.lstm = nn.LSTM(input_size=self.embedding_dim, hidden_size=self.hidden_dim)
+        self.GRU = nn.GRU(input_size=self.embedding_dim, hidden_size=self.hidden_dim, dropout=dropout, num_layers=2)
         
     def forward(self, input_sequence, max_len):
 #         print(input_sequence)
 #         print(max_len)
         embedded = self.embedding(input_sequence)
         packed = nn.utils.rnn.pack_padded_sequence(embedded, max_len, batch_first=True)
-        outputs, (self.c_n, self.h_n) = self.lstm(packed)
+        outputs, self.h_n = self.GRU(packed)
         # Unpack padding
         """
             Honestly, I do not know if at this point, I need the output. I would rather prefer to work with the
@@ -30,7 +30,7 @@ class EmbeddingNetwork(nn.Module):
         # on which output should the prediction be done?
         # self.h_n =  num_layers, batch_size, hidden_dim
         batch_size = self.h_n.shape[1]
-        final_hidden_state = self.h_n.reshape(batch_size, -1)
+        final_hidden_state = self.h_n[-1].view(batch_size, -1)  # Taking the output from the last stacked layer
         return final_hidden_state, sequence_embedded
     
 class ClassificationNetwork(nn.Module):
@@ -39,12 +39,16 @@ class ClassificationNetwork(nn.Module):
     def __init__(self, embedding_model, hidden_dim, num_classes):
         super(ClassificationNetwork, self).__init__()
         self.embedding_model = embedding_model
-        self.linear = nn.Linear(hidden_dim, num_classes)
+        self.dropout = nn.Dropout()
+        self.bn = nn.BatchNorm1d(num_classes * 2)
+        self.relu = nn.ReLU()
+        self.linear_first = nn.Linear(hidden_dim, num_classes * 2)
+        self.linear = nn.Linear(num_classes * 2, num_classes)
 
     def forward(self, input_sequence, max_len):
         
         final_hidden_state, _ = self.embedding_model(input_sequence, max_len)
-        output_predicted = self.linear(final_hidden_state)
+        output_predicted = self.linear(self.dropout(self.bn(self.relu(self.linear_first(final_hidden_state)))))
         return output_predicted
 
     def get_embedder(self):
